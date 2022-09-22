4d0dba35374536df54c385d5a6ab64cceafcf5de
ett0n <mathieu.notte6@gmail.com>
2022-09-22 13:23:14 +0200

    blank

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 import "aframe";
 
-import { Scene, Assets, AssetItem, Box, Sphere, Cylinder, Entity, Plane, Sky } from "@belivvr/aframe-react";
-
 function App() {
   const [count, setCount] = useState(0);
 
@@ -12,14 +10,19 @@ function App() {
 
   return (
     <div className="App">
-      <Scene>
+      {/* <Scene>
         <Box position={{ x: -1, y: 0.5, z: -3 }} rotation={{ x: 0, y: 45, z: 0 }} color="#4CC3D9" />
         <Sphere position={{ x: 0, y: 1.25, z: -5 }} radius={1.25} color="#EF2D5E" />
         <Cylinder position={{ x: 1, y: 0.75, z: -3 }} radius={0.5} height={1.5} color="#FFC65D" />
         <Plane position={{ x: 0, y: 0, z: -4 }} rotation={{ x: -90, y: 0, z: 0 }} width={4} height={4} color="#7BC8A4" />
         <Entity position={{ x: 0, y: 0, z: -4 }} gltfModel={"/speculMan.glb"} width={4} scale={{ x: xSize, y: xSize, z: xSize }} animation={{ property: "scale", to: "" }}></Entity>
         <Sky src={"/assets/sky2.png"} />
-      </Scene>
+      </Scene> */}
+      <a-scene>
+        <a-box src="https://i.imgur.com/mYmmbrp.jpg" position="0 2 -5" rotation="0 45 45" scale="2 2 2"></a-box>
+        <a-sky color="#222" src="/assets/sky2.png"></a-sky>
+        <a-entity position="0 0 1" gltfModel="/speculMan.glb" width="1" scale="1 1 1"></a-entity>
+      </a-scene>
     </div>
   );
 }
